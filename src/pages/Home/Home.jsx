37e7c8973b141b0eca06a70b7d23649c2b0e5a22
Baburaj <sbrbabu123@gmail.com>
2025-01-27 01:17:10 +0530

    hero section created

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,25 @@
 import Footer from "../../components/Footer/Footer";
 import Navbar from "../../components/Navbar/Navbar";
+import HSImg from "../../assets/herosection.png"
+import './Home.css'
 
 const Home = () => {
     return(
         <>
            <Navbar/>
-           <div className="home" style={{"border" : "1px red solid", "padding" : "50%"}}>
-            Home page
+           <div className="hero-section">
+                <div className="hero-section-content">
+                    <h1 className="slogan">
+                        Building tomorrow's solution today itself!
+                    </h1>
+                    <p className="sub-slogan">
+                        A team of young passionate tech enthusiats
+                    </p>
+                    
+                </div>
+                <div className="hero-image">
+                        <img src={HSImg} />
+                    </div>
            </div>
            <Footer/>
         </>
