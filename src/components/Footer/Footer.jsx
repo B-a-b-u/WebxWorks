508a96801ded3e1308b5188c381cd60880224991
Baburaj <sbrbabu123@gmail.com>
2025-01-27 00:50:37 +0530

    footer created

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,37 @@
-
+import { NavLink } from 'react-router-dom';
+import './Footer.css'
+import { SocialIcon } from 'react-social-icons';
+import Logo from "../../assets/favicon.png"
 
 const Footer = () => {
     return(
         <>
-            <p>Footer page</p>
+            <div className="footer-container">
+                <div className="reactout">
+                    <h3 className="section-heading">Address</h3>
+                    <p>69, Mannar kudi street</p>
+                    <p> Dubai kuruku santhu</p>
+                    <p>dubai </p>
+                    <h3 className="contact-num">Contact</h3>
+                    <p>9994416162</p>
+                </div>
+                <div className="company-links">
+                    <h3 className='section-heading'>Explore</h3>
+                    <NavLink to="/">Home</NavLink>
+                    <NavLink to="/about">About</NavLink>
+                    <NavLink to="/services">Services</NavLink>
+                    <NavLink to="/contact">Contact</NavLink>
+                </div>
+                <div className="follow-links">
+                    <h3 className="links">Reach out</h3>
+                    {/* <NavLink to="/" className="logo"><img src={Logo} alt="" /></NavLink> */}
+                    <SocialIcon url="www.x.com" />
+                    <SocialIcon url="www.instagram.com" />
+                    <SocialIcon url="www.linkedin.com" />
+                    <SocialIcon url="www.google.com" />
+                </div>
+
+            </div>
         </>
     )
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,14 @@
+import Footer from "../../components/Footer/Footer";
 import Navbar from "../../components/Navbar/Navbar";
 
 const Home = () => {
     return(
         <>
            <Navbar/>
+           <div className="home" style={{"border" : "1px red solid", "padding" : "50%"}}>
+            Home page
+           </div>
+           <Footer/>
         </>
     )
 }
