c5a70c9d248ee9dfd1d8d0db3e19b1e858cc579e
Baburaj <sbrbabu123@gmail.com>
2025-01-28 16:29:08 +0530

    about added with images and content

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,6 +1,8 @@
 import Navbar from "../../components/Navbar/Navbar";
 import Footer from "../../components/Footer/Footer";
 import AboutImg from "../../assets/about.png";
+import About2 from "../../assets/about3.png";
+
 import "./About.css";
 
 const About = () => {
@@ -8,28 +10,32 @@ const About = () => {
         <>
             <Navbar />
             <div className="about-section">
-                <div className="about-img">
-                    <h1 className="about-heading">
-                        Student minds, endless possibiities
-                    </h1>
-                    <img src={AboutImg} alt="" />
-                </div>
+            <div className="about-section1">
                 <div className="about-content">
-                    We are a team of computer science
-                    engineering students brought together
-                    by Our shared passion for technology anc
-                    innovation. What started as a g roup of
-                    classmates has evol•ved into a close-knit
-                    team. learning, collaborating. and
-                    growing together.
-                    Ou journey is fueled by curi.osity,
-                    creativity, and a drrve to make a
-                    difference. Join us as vee continue to
-                    explore new technologies, build
-                    meaningful projects and shape the
-                    rutUre or technology.
+                <h1 className="about-heading">
+                        About Us
+                </h1>
+                <div className="about-subcontent">
+                We are a passionate team of young tech enthusiasts driven by our love for technology and innovation. We began as a group of curious learners has evolved into a cohesive team dedicated to exploring cutting-edge solutions and building meaningful projects. With diverse skills and a shared vision, we bring creativity and technical expertise to every project we undertake. Our journey is not just about coding but about fostering a culture of innovation, teamwork, and continuous learning. We are committed to solving real-world problems, contributing to the tech community, and making a positive impact on society.
                 </div>
-            
+                </div>
+                <div className="about-img-container">
+                <img className="about-img" src={AboutImg} alt="" />
+                </div>
+            </div>
+            <div className="about-section2">
+                <div className="about-content">
+                <h1 className="about-heading">
+                        Young minds, endless possibiities
+                </h1>
+                <div className="about-subcontent">
+                Our journey is fueled by creativity, curiosity, and an unrelenting drive to make a difference. We specialize in crafting innovative web solutions, developing applications, and experimenting with emerging technologies. By combining technical expertise with teamwork, we aim to solve real-world problems and shape the future of technology. Join us as we continue to innovate and grow.
+                </div>
+                </div>
+                <div className="about-img-container">
+                <img className="about-img" src={About2} alt="" />
+                </div>
+            </div>
             </div>
             <Footer />
         </>
